fad4936522c32ac9a4e07a2d31ec131d5d8ffbd4
Alexey <91435704+LAGNIZE@users.noreply.github.com>
2021-12-26 17:45:32 +0300

    Add files via upload

--- Practise/Vue Project/js/Background.js
+++ Practise/Vue Project/js/Background.js
@@ -1,9 +1,10 @@
 class Background {
     constructor(width, height, background) {
         this.dom = document.querySelector('body')
-        this.dom.style.width = width + 'vw';
-        this.dom.style.height = height + 'vh';
+        this.dom.style.width = width + '%';
+        this.dom.style.height = height + '%';
         this.dom.style.background = background;
+        this.dom.style.backgroundSize = "cover";
         this.width = width;
         this.height = height;
     }
